2af2b9ddc89c85a5673654a5674b9f45962d5fdb
MonisMS <monissms16@gmail.com>
2025-03-11 00:01:46 +0530

    Added JWT, DB error handling, and signup logic

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from "express"
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/authroutes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app=express();
-dotenv.config()
+
+
 const PORT =process.env.PORT || 5000;
-app.get("/",(req,res)=>{
-    //root route http://localhost:5000/
-    res.send("hellooooo");
-});
+dotenv.config()
+
+app.use(express.json());  //to parse the incoming request with JSON pyload (from req.body)
 
 app.use("/api/auth",authRoutes)
+
+//app.get("/",(req,res)=>{
+    //root route http://localhost:5000/
+   // res.send("hellooooo");
+//});
+
 
 
 app.listen(PORT,()=>{
