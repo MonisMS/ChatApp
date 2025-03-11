07373d51a84463ee7c51e9975d8175bb8c64fd98
MonisMS <monissms16@gmail.com>
2025-03-12 03:01:39 +0530

    Styled and improved login, signup, and home pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Login from './pages/login/Login'
+import SignUp from './pages/signup/signup'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        <Login/>
+        <SignUp/>
       </div>
     </>
   )
