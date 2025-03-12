cff9a2a02e4c7013d80f9ecb68e4bd85b40aaf48
MonisMS <monissms16@gmail.com>
2025-03-12 17:41:24 +0530

    Updated UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@
 import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/signup'
+import Home from './pages/home/Home'
 function App() {
   
 
   return (
     <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        <SignUp/>
+        <Home/>
       </div>
     </>
   )
